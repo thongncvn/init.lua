de0360bdff5cee8a214f542267784f0956614867
Thong Nguyen <thong.nguyencao.vn@gmail.com>
2025-01-23 20:01:34 +0700

    Update plugins, and add keymaps to LSP

--- lua/plugins/mason-with-lspconfig.lua
+++ lua/plugins/mason-with-lspconfig.lua
@@ -1,12 +1,17 @@
 return {
   "williamboman/mason.nvim",
+  lazy = false,
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   },
+  keys = {
+    { '<leader>cf', '<cmd>lua vim.lsp.buf.format()<cr>', desc = 'LSP format' },
+    { '<leader>cd', '<cmd>lua vim.lsp.buf.definition()<cr>', desc = 'LSP definition' },
+  },
   config = function()
-    require('mason').setup()
-    require('mason-lspconfig').setup()
+    require('mason').setup({})
+    require('mason-lspconfig').setup({})
 
     require("mason-lspconfig").setup_handlers({
       -- Default
